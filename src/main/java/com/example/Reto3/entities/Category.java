9fb9811c2964103011e563d871bf0a9beac04e16
patman89 <75132741+patman89@users.noreply.github.com>
2022-10-04 20:16:34 -0500

    Creacion-Client-controller,repository, servicios

---
+++ src/main/java/com/example/Reto3/entities/Category.java
@@ -0,0 +1,56 @@
+package com.example.Reto3.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+@Entity
+@Table(name = "category")
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCategory;
+    private String name;
+    private String description;
+
+
+    @OneToMany(cascade = {CascadeType.PERSIST},mappedBy = "category")
+    @JsonIgnoreProperties("category")
+
+    private List<Game> game;
+
+
+    public Integer getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(Integer id) {
+        this.idCategory = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Game> getGame() {
+        return game;
+    }
+
+    public void setGame(List<Game> game) {
+        this.game = game;
+    }
+}
---
+++ src/main/java/com/example/Reto3/repository/crudRepository/CategoryCrudRepository.java
@@ -0,0 +1,7 @@
+package com.example.Reto3.repository.crudRepository;
+
+import com.example.Reto3.entities.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryCrudRepository extends CrudRepository<Category,Integer> {
+}
