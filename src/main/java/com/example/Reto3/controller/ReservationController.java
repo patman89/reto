deea13949947bc3aad2fef00750d60c3aaeafb10
patman89 <75132741+patman89@users.noreply.github.com>
2022-10-07 19:09:35 -0500

    Creacion-Client-controller,repository, servicios

--- src/main/java/com/example/Reto3/controller/ReservationController.java
+++ src/main/java/com/example/Reto3/controller/ReservationController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController()
 @RequestMapping("/api/Reservation")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class ReservationController {
     @Autowired
     private ReservationService reservationService;
